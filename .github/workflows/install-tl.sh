85bb50d70634918caa9dc7a07eca465ff9c99049
易程 <songyicheng0@gmail.com>
2020-05-10 17:55:42 +0800

    [Change] commit old packages & update '.gitignore'

--- .github/workflows/install-tl.sh
+++ .github/workflows/install-tl.sh
@@ -16,10 +16,9 @@ HYPERREF_PKGS="bitset letltxmacro pdfescape pdflscape";
 NOMENCL_PKGS="nomencl koma-script xkeyval";
 
 BIN_PKGS="latexmk l3build";
-REQUIRED_PKGS="$XETEX_PKGS $CTEX_PKGS amsmath amsfonts amsthm amssymb amsbsy \
-  bm mathrsfs";
+REQUIRED_PKGS="$XETEX_PKGS $CTEX_PKGS amsmath amsfonts";
 FONT_PKGS="fandol";
-EXTRA_PKGS="$HYPERREF_PKGS $NOMENCL_PKGS siunitx graphicx";
-DOC_PKGS="booktabs listings color xcolor";
+EXTRA_PKGS="$HYPERREF_PKGS $NOMENCL_PKGS siunitx";
+DOC_PKGS="booktabs listings xcolor";
 
 tlmgr install $BIN_PKGS $REQUIRED_PKGS $FONT_PKGS $EXTRA_PKGS $DOC_PKGS;
