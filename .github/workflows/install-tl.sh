f6b95f9115e42e6987fad6db8c1e841d7a60cfa5
易程 <songyicheng0@gmail.com>
2020-05-11 11:13:16 +0800

    [Modified] 使用'xu-cheng/texlive-action'进行测试

--- .github/workflows/install-tl.sh
+++ .github/workflows/install-tl.sh
@@ -1,14 +1,3 @@
-REMOTE="https://mirrors.tuna.tsinghua.edu.cn/CTAN/systems/texlive/tlnet/";
-INSTALL="/tmp/install-texlive";
-
-mkdir -p "$INSTALL";
-curl -sSL "$REMOTE/install-tl-unx.tar.gz" | tar -xz -C "$INSTALL" \
-    --strip-components=1;
-"$INSTALL/install-tl" -no-gui -repository $REMOTE \
-    -profile .github/workflows/texlive.profile;
-
-export PATH="/tmp/texlive/bin/x86_64-linux:$PATH";
-
 # package requirement
 XETEX_PKGS="fontname fontspec l3packages xetex";
 CTEX_PKGS="cjk ctex environ ms trimspaces ulem xecjk zhnumber";
